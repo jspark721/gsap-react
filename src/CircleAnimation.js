386b2370726f9821073ef427d5d9d41307f179ed
jspark721 <jspark721@gmail.com>
2021-03-02 17:26:16 -0800

    circle animations with tween max using useRef & useState

---
+++ src/CircleAnimation.js
@@ -0,0 +1,70 @@
+import React, { useEffect, useRef, useState } from "react";
+
+import { TweenMax, Power3 } from "gsap";
+import "./App.css";
+
+function CircleAnimation() {
+  let app = useRef(null);
+  let circle = useRef(null);
+  let circleRed = useRef(null);
+  let circleBlue = useRef(null);
+
+  const [state, setState] = useState(false);
+
+  const handleExpand = () => {
+    TweenMax.to(circleRed, 0.8, {
+      width: 200,
+      height: 200,
+      ease: Power3.easeOut,
+    });
+    setState(true);
+  };
+  const handleDecrease = () => {
+    TweenMax.to(circleRed, 0.8, {
+      width: 75,
+      height: 75,
+      ease: Power3.easeOut,
+    });
+    setState(false);
+  };
+  useEffect(() => {
+    TweenMax.to(app, 0, { css: { visibility: "visible" } });
+    // TweenMax.from(circle, 0.8, { opacity: 0, x: 40, ease: Power3.easeOut });
+    // TweenMax.from(circleRed, 0.8, {
+    //   opacity: 0,
+    //   x: 40,
+    //   ease: Power3.easeOut,
+    //   delay: 0.2,
+    // });
+    // TweenMax.from(circleBlue, 0.8, {
+    //   opacity: 0,
+    //   x: 40,
+    //   ease: Power3.easeOut,
+    //   delay: 0.4,
+    // });
+    TweenMax.staggerFrom(
+      [circle, circleRed, circleBlue],
+      0.8,
+      { opacity: 0, x: 40, ease: Power3.easeOut },
+      0.2
+    );
+  }, []);
+
+  return (
+    <div ref={(el) => (app = el)} className="CircleApp">
+      <header className="App-header">
+        <div className="circle-container">
+          <div ref={(el) => (circle = el)} className="circle"></div>
+          <div
+            onClick={state !== true ? handleExpand : handleDecrease}
+            ref={(el) => (circleRed = el)}
+            className="circle red"
+          ></div>
+          <div ref={(el) => (circleBlue = el)} className="circle blue"></div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default CircleAnimation;
