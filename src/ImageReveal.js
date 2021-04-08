4a2346aba2127aeb2426608a84669f39de3439ba
jspark721 <jspark721@gmail.com>
2021-04-08 13:53:58 -0700

    image reveal animation

--- src/ImageReveal.js
+++ src/ImageReveal.js
@@ -1,15 +1,27 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import "./App.scss";
-import { TweenMax, Power3 } from "gsap";
 import Seattle from "./images/felipe-galvan-seattle.jpg";
+import CSSRulePlugin from "gsap/CSSRulePlugin";
+import { TimelineLite, Power2 } from "gsap";
 
 const ImageReveal = () => {
+  let container = useRef(null);
+  let image = useRef(null);
+  let imageReveal = CSSRulePlugin.getRule(".img-container:after");
+
+  const tl = new TimelineLite();
+
+  useEffect(() => {
+    tl.to(container, 0, { css: { visibility: "visible" } })
+      .to(imageReveal, 1.4, { width: "0%", ease: Power2.easeInOut })
+      .from(image, 1.4, { scale: 1.6, ease: Power2.easeInOut, delay: -1.6 });
+  });
   return (
     <section className="main">
-      <div className="container">
+      <div ref={(el) => (container = el)} className="container">
         <>
           <div className="img-container">
-            <img src={Seattle} />
+            <img ref={(el) => (image = el)} src={Seattle} />
           </div>
         </>
       </div>
